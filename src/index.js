94d86532499a745adf78810aaa36d3a98ccee1cc
jordendickerson <jordendickerson24@gmail.com>
2022-08-01 14:30:23 -0600

    Moved the nav bar to its own module:

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
 require('./styles/main.scss');
+import navBar from './navigation/nav-bar';
 
 const contentDiv = document.getElementById('content')
 
 function app(){
-    //create nav bar wrapper
-    const navWrapper = document.createElement('div');
-    navWrapper.classList.add('nav-wrapper');
+    //create app div
+    const app = document.createElement('div');
+    app.classList.add('app');
 
-    //create logo in nav bar
-    const navLogo = document.createElement('img');
-    navLogo.setAttribute("src", "../src/microfood.svg");
-    navLogo.classList.add('logo');
+    app.appendChild(navBar());
 
-    //nav wrappers
-    const navButtonWrapper1 = document.createElement('div');
-    navButtonWrapper1.classList.add('nav-button-wrapper');
-
-    const navButtonWrapper2 = document.createElement('div');
-    navButtonWrapper2.classList.add('nav-button-wrapper');
-
-    //create menu button
-    const menuButton = document.createElement('div');
-    menuButton.classList.add('nav-button');
-    menuButton.innerText = 'Menu';
-
-    //create about button
-    const aboutButton = document.createElement('div');
-    aboutButton.classList.add('nav-button');
-    aboutButton.innerText = 'About';
-
-    //append nav bar elements to navWrapper
-    navWrapper.appendChild(navLogo);
-    navWrapper.appendChild(navButtonWrapper1);
-    navWrapper.appendChild(navButtonWrapper2);
-    navButtonWrapper1.appendChild(menuButton);
-    navButtonWrapper2.appendChild(aboutButton);
-    
-    return navWrapper;
+    return app;
 }
 
 contentDiv.appendChild(app());
