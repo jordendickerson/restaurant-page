2eae1524fa9f21b5ebebe3190e588af8c567ef1f
jordendickerson <jordendickerson24@gmail.com>
2022-08-01 14:03:23 -0600

    Created Nav Bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('test from index.js!!!');
+require('./styles/main.scss');
+
+const contentDiv = document.getElementById('content')
+
+function app(){
+    //create nav bar wrapper
+    const navWrapper = document.createElement('div');
+    navWrapper.classList.add('nav-wrapper');
+
+    //create logo in nav bar
+    const navLogo = document.createElement('img');
+    navLogo.setAttribute("src", "../src/microfood.svg");
+    navLogo.classList.add('logo');
+
+    //nav wrappers
+    const navButtonWrapper1 = document.createElement('div');
+    navButtonWrapper1.classList.add('nav-button-wrapper');
+
+    const navButtonWrapper2 = document.createElement('div');
+    navButtonWrapper2.classList.add('nav-button-wrapper');
+
+    //create menu button
+    const menuButton = document.createElement('div');
+    menuButton.classList.add('nav-button');
+    menuButton.innerText = 'Menu';
+
+    //create about button
+    const aboutButton = document.createElement('div');
+    aboutButton.classList.add('nav-button');
+    aboutButton.innerText = 'About';
+
+    //append nav bar elements to navWrapper
+    navWrapper.appendChild(navLogo);
+    navWrapper.appendChild(navButtonWrapper1);
+    navWrapper.appendChild(navButtonWrapper2);
+    navButtonWrapper1.appendChild(menuButton);
+    navButtonWrapper2.appendChild(aboutButton);
+    
+    return navWrapper;
+}
+
+contentDiv.appendChild(app());
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,23 @@ module.exports = {
         {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: "asset/resource"
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            {
+              loader: "sass-loader",
+              options: {
+                // Prefer `dart-sass`
+                implementation: require.resolve("sass"),
+              },
+            }
+          ],
         }
     ]
   }
